61ac01aead8cce99661eb4f6c0d18239b71e0bfc
Monica-53 <W0632746@myscc.ca>
2022-11-21 15:43:07 -0500

    CategoryRepository.cs created

---
+++ EdaMonicaBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using EdaMonicaBookStore.DataAccess.Repository.IRepository;
+using EdaMonicaBookStore.Models;
+using EdaMonicaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdaMonicaBookStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            //use .NET LINQ to retreive the first or default category object
+            // then pass the id as a generic entity which matches the category
+        }
+    }
+}
---
+++ EdaMonicaBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdaMonicaBookStore.DataAccess.Repository.IRepository
+{
+    class ICategoryRepository
+    {
+
+    }
+}
