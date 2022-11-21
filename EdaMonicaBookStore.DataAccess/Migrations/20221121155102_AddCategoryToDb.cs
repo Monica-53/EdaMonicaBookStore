081f3ee9b5a5c47042c17daff654cef62b2caa47
W0632746 <W0632746@ADSOL0045L-11>
2022-11-21 12:47:05 -0500

    updated

---
+++ EdaMonicaBookStore.DataAccess/Migrations/20221121155102_AddCategoryToDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EdaMonicaBookStore.DataAccess.Migrations
+{
+    public partial class AddCategoryToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+       
+    }
+}
