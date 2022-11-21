fac31b6a0e9035d01ad9adc737a79875dece2d30
W0632746 <W0632746@ADSOL0045L-11>
2022-11-21 13:56:12 -0500

    migration AfterADDCategoryModel

--- EdaMonicaBookStore.DataAccess/Migrations/20221121185344_MigrationAfterAddCategoryModel.Designer.cs
+++ EdaMonicaBookStore.DataAccess/Migrations/20221121185344_MigrationAfterAddCategoryModel.Designer.cs
@@ -4,14 +4,16 @@ using EdaMonicaBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EdaMonicaBookStore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221121185344_MigrationAfterAddCategoryModel")]
+    partial class MigrationAfterAddCategoryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
