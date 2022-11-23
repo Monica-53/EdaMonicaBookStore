43032834c6fcfab90363b67bd32c181ced40367f
Monica-53 <99419818+Monica-53@users.noreply.github.com>
2022-11-23 17:41:10 -0500

    updated

--- EdaMonicaBookStore.DataAccess/Repository/CategoryRepository.cs
+++ EdaMonicaBookStore.DataAccess/Repository/CategoryRepository.cs
@@ -3,6 +3,7 @@ using EdaMonicaBookStore.Models;
 using EdaMonicaBookStore.DataAccess.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EdaMonicaBookStore.DataAccess.Repository
--- EdaMonicaBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ EdaMonicaBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using EdaMonicaBookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EdaMonicaBookStore.DataAccess.Repository.IRepository
 {
-    class ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
-
+        void Update(Category category);
     }
 }
