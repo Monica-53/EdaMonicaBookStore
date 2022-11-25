7bca74859c303594ea8f0797b3430956b39ebb8c
W0632746 <W0632746@ADSOL0045N-04>
2022-11-25 12:44:32 -0500

    CategoryController.cs

--- EdaMonicaBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ EdaMonicaBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -22,15 +22,39 @@ namespace EdaMonicaBookStore.Areas.Admin.Controllers
             return View();
         }
 
-        //SPI calls here
+        public IActionResult Upsert(int? id) //action method for Upsert
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                //this is for create
+                return View(category);
+            }
+
+            //this is for edit
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+            //API calls here
+        }
+
+
+        //API calls here
         #region API CALLS
         public IActionResult GetAll()
         {
-        //return NotFound();
+            //return NotFound();
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
         }
         #endregion
+
+      
     }
+
 }
 
